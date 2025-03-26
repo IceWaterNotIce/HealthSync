0d39cbfceb49cd6bc6aabee4a3df13c063e2a7cf
冰水不冰 <icewaternotice@gmail.com>
2025-03-26 15:40:45 +0800

    新增步數計數按鈕至 SportActivity，並設置點擊事件以啟動 StepCounterActivity

--- app/src/main/java/com/example/healthsync/SportActivity.java
+++ app/src/main/java/com/example/healthsync/SportActivity.java
@@ -1,6 +1,9 @@
 package com.example.healthsync;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class SportActivity extends BaseActivity {
 
@@ -18,5 +21,13 @@ public class SportActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // ...新增 SportActivity 的邏輯...
+
+        Button stepCounterButton = findViewById(R.id.btnStepCounter);
+        stepCounterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SportActivity.this, StepCounterActivity.class));
+            }
+        });
     }
 }
