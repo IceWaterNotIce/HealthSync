789e94b3ceae44914d658419d3950c83bef444b2
冰水不冰 <icewaternotice@gmail.com>
2025-03-27 12:31:19 +0800

    新增運動目標卡路里設定功能，包含 SeekBar 和顯示文本

--- app/src/main/java/com/icewaternotice/healthsync/SportActivity.java
+++ app/src/main/java/com/icewaternotice/healthsync/SportActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SportActivity extends BaseActivity {
 
@@ -27,6 +29,26 @@ public class SportActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(SportActivity.this, StepCounterActivity.class));
+            }
+        });
+
+        TextView tvSportTarget = findViewById(R.id.tvSportTarget);
+        SeekBar seekBarSportTarget = findViewById(R.id.seekBarSportTarget);
+
+        seekBarSportTarget.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvSportTarget.setText("運動目標卡路里: " + progress + " kcal");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Do nothing
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Do nothing
             }
         });
     }
