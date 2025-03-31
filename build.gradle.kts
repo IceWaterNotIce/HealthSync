57bbff510ff6d4e6f3e8c0c612505243afe2c4f3
冰水不冰 <icewaternotice@gmail.com>
2025-04-01 04:09:38 +0800

    更新 Firebase 依賴版本，新增 Firebase Analytics 支援

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,5 +12,5 @@ buildscript {
 
 plugins {
     alias(libs.plugins.androidApplication) apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
